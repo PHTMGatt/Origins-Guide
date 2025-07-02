9936113bdcd9e4a882dd2f2f0e7e85d420d4b0f0
PHTMGatt <phtmgatt@gmail.com>
2025-07-02 13:50:06 -0400

    Fix global.css path for case-sensitive deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/global.css";
+import "./Styles/global.css";
 
 const root = document.getElementById("root");
 
